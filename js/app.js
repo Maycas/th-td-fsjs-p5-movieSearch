9c9f9609f8e2cbf61e72d3c9d23def889fb8bdd6
Marc Maycas <marcmaycas@Marcs-MacBook-Pro.local>
2016-10-12 00:30:18 +0200

    Movie Detail Page added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ var app = (function ($) {
 
     var app = {
         omdbAPI: "https://www.omdbapi.com/?",
+        imdbBaseURL: "http://www.imdb.com/title/",
         $movieList: $("#movies"),
+        $movieDetail: $("#movie-detail"),
+        movieDetails: {
+            $movieTitle: $(".movie-detail-title"),
+            $movieRating: $(".movie-detail-rating span"),
+            $moviePoster: $(".movie-detail-poster img"),
+            $movieSynopsis: $("#movie-detail-synopsis"),
+            $imdb: $(".view-imdb")
+        },
 
         getTitle: function () {
             return $("#search").val();
@@ -23,6 +32,14 @@ var app = (function ($) {
             }).done(this.printMoviesResults).fail(this.showError);
         },
 
+        searchMovieDetails: function (title) {
+            $.getJSON(this.omdbAPI, {
+                t: title,
+                plot: "short",
+                type: "movie"
+            }).done(this.printMovieDetails).fail(this.showError);
+        },
+
         printMoviesResults: function (movies) {
             var html = "";
 
@@ -35,6 +52,7 @@ var app = (function ($) {
 
                 $.each(moviesArray, function (idx, movie) {
                     html += "<li>";
+                    html += "<a href='#'>";
                     html += "<div class='poster-wrap'>";
 
                     // Check if the poster is available
@@ -51,32 +69,92 @@ var app = (function ($) {
                     // Add movie year
                     html += "<span class='movie-year'>" + movie.Year + "</span>";
 
+                    html += "</a>";
                     html += "</li>";
                 });
             }
 
             // Append the result to the html
-            app.$movieList.append(html);
+            $("#movies").append(html);
+        },
+
+        printMovieDetails: function (movie) {
+            if (movie.Response === "False") {
+                alert("Something went wrong. Please refresh the page");
+            } else {
+                var plot;
+
+                // Hide the list
+                app.$movieList.hide();
+
+                // Fill in the data inside the movie details page
+                app.movieDetails.$movieTitle.text(movie.Title + " (" + movie.Year + ")");
+
+                app.movieDetails.$movieRating.text(movie.imdbRating);
+
+                if (movie.Poster !== "N/A") {
+                    app.movieDetails.$moviePoster.attr("src", movie.Poster);
+                } else {
+                    // TODO: When there's no poster needs a fix. CSS also needs a fix on image size
+                }
+
+                if (movie.Plot === "N/A") {
+                    plot = "There's no synopsis available for this movie";
+                } else {
+                    plot = movie.Plot;
+                }
+                app.movieDetails.$movieSynopsis.text(plot);
+
+                app.movieDetails.$imdb.attr("href", app.imdbBaseURL + movie.imdbID);
+
+                // Show the details
+                app.$movieDetail.show();
+            }
         },
 
         showError: function () {
             alert("Something went wrong with your request. Please refresh your browser");
         },
 
-        registerListeners: function () {
+        registerMainMovieListListeners: function () {
             // Submit the form
             $("#submit").click(function (event) {
                 event.preventDefault();
+
+                // Hide the movie detail and show the list
+                app.$movieDetail.hide();
+                app.$movieList.show();
+
+                // Reset the list
                 app.$movieList.html("");
+
+                // Build the new list
                 app.searchMovies();
+            });
+
+            this.$movieList.on("click", "li a", function (event) {
+                var clickedTitle = $(this).children(".movie-title").text();
+                app.searchMovieDetails(clickedTitle);
+            });
+        },
+
+        registerMovieDetailListeners: function () {
+            // Back button
+            $("#back").click(function (event) {
+                event.preventDefault();
+                app.$movieDetail.hide();
+                app.$movieList.show();
             });
         },
 
         init: function () {
-            this.registerListeners();
-        }
+            this.$movieList.show();
+            this.registerMainMovieListListeners();
 
+            this.registerMovieDetailListeners();
+        }
     };
+
 
     app.init();
 
